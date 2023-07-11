e2a76d361dbddaeddea0dcdd48548b9c7bfb4359
[Daniel Sun] <dansun@stanford.edu>
2023-07-11 16:59:23 -0700

    Create runs

---
+++ test_18.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+#SBATCH --job-name=test_job
+#SBATCH --output=test_job.%j.out
+#SBATCH --error=test_job.%j.err
+#SBATCH --time=12:00:00
+#SBATCH -p normal
+#SBATCH -c 1
+#SBATCH --mem=4GB
+#SBATCH --mail-type=ALL
+
+python3 -u test.py -n 512 -T 150 -p 0.18 -s 100
--- test_8.sh
+++ test_8.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=test_job
 #SBATCH --output=test_job.%j.out
 #SBATCH --error=test_job.%j.err
-#SBATCH --time=01:00:00
+#SBATCH --time=12:00:00
 #SBATCH -p normal
-#SBATCH -c 4
-#SBATCH --mem=16GB
+#SBATCH -c 1
+#SBATCH --mem=4GB
 #SBATCH --mail-type=ALL
 
-python3 -u test.py -n 512 -T 150 -p 0.06 -s 4
+python3 -u test.py -n 512 -T 150 -p 0.08 -s 100
