4cc167abd93ac631c3f0acc7309ff53a4856e66e
[Daniel Sun] <dansun@stanford.edu>
2023-07-20 13:43:04 -0700

    fix typo in hist slurm file, fix plotting nb

--- entropy_hist.sh
+++ entropy_hist.sh
@@ -9,14 +9,14 @@
 #SBATCH --mem-per-cpu=1G
 #SBATCH --mail-type=ALL
 
-srun --exclusive -n 1 -N 1 -c 1 python3 entropy_time.py -n 512 -T 256 -p 0.02 -s 1000 &
-srun --exclusive -n 1 -N 1 -c 1 python3 entropy_time.py -n 512 -T 256 -p 0.04 -s 1000 &
-srun --exclusive -n 1 -N 1 -c 1 python3 entropy_time.py -n 512 -T 256 -p 0.06 -s 1000 &
-srun --exclusive -n 1 -N 1 -c 1 python3 entropy_time.py -n 512 -T 256 -p 0.08 -s 1000 &
-srun --exclusive -n 1 -N 1 -c 1 python3 entropy_time.py -n 512 -T 256 -p 0.10 -s 1000 &
-srun --exclusive -n 1 -N 1 -c 1 python3 entropy_time.py -n 512 -T 256 -p 0.12 -s 1000 &
-srun --exclusive -n 1 -N 1 -c 1 python3 entropy_time.py -n 512 -T 256 -p 0.14 -s 1000 &
-srun --exclusive -n 1 -N 1 -c 1 python3 entropy_time.py -n 512 -T 256 -p 0.16 -s 1000 &
-srun --exclusive -n 1 -N 1 -c 1 python3 entropy_time.py -n 512 -T 256 -p 0.18 -s 1000 &
-srun --exclusive -n 1 -N 1 -c 1 python3 entropy_time.py -n 512 -T 256 -p 0.20 -s 1000 &
+srun --exclusive -n 1 -N 1 -c 1 python3 entropy_hist.py -n 512 -T 256 -p 0.02 -s 1000 &
+srun --exclusive -n 1 -N 1 -c 1 python3 entropy_hist.py -n 512 -T 256 -p 0.04 -s 1000 &
+srun --exclusive -n 1 -N 1 -c 1 python3 entropy_hist.py -n 512 -T 256 -p 0.06 -s 1000 &
+srun --exclusive -n 1 -N 1 -c 1 python3 entropy_hist.py -n 512 -T 256 -p 0.08 -s 1000 &
+srun --exclusive -n 1 -N 1 -c 1 python3 entropy_hist.py -n 512 -T 256 -p 0.10 -s 1000 &
+srun --exclusive -n 1 -N 1 -c 1 python3 entropy_hist.py -n 512 -T 256 -p 0.12 -s 1000 &
+srun --exclusive -n 1 -N 1 -c 1 python3 entropy_hist.py -n 512 -T 256 -p 0.14 -s 1000 &
+srun --exclusive -n 1 -N 1 -c 1 python3 entropy_hist.py -n 512 -T 256 -p 0.16 -s 1000 &
+srun --exclusive -n 1 -N 1 -c 1 python3 entropy_hist.py -n 512 -T 256 -p 0.18 -s 1000 &
+srun --exclusive -n 1 -N 1 -c 1 python3 entropy_hist.py -n 512 -T 256 -p 0.20 -s 1000 &
 wait
