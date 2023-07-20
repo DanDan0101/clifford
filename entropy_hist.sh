358981bb40a6da6270521fafd7631ce2abe5c705
Daniel Sun <dansun@stanford.edu>
2023-07-20 14:33:52 -0700

    increase memory for hist

--- entropy_hist.sh
+++ entropy_hist.sh
@@ -6,7 +6,7 @@
 #SBATCH -p hns
 #SBATCH --ntasks=10
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=1G
+#SBATCH --mem-per-cpu=2G
 #SBATCH --mail-type=ALL
 
 srun --exclusive -n 1 -N 1 -c 1 python3 entropy_hist.py -n 512 -T 256 -p 0.02 -s 1000 &
