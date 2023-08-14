8dc0f4d81e486bbc88a5e12a19f39e98c512da60
Daniel Sun <dansun@stanford.edu>
2023-08-14 12:56:46 -0700

    D=1

--- entropies.sh
+++ entropies.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
 #SBATCH --job-name=entropies
+#SBATCH --open-mode=append
 #SBATCH --output=entropies.out
 #SBATCH --error=entropies.err
-#SBATCH --time=6:00:00
+#SBATCH --time=1:00:00
 #SBATCH -p hns
-#SBATCH --array=1-24
+#SBATCH --array=1-6
 #SBATCH -c 16
-#SBATCH --mem-per-cpu=4G
+#SBATCH --mem-per-cpu=2G
 #SBATCH --mail-type=ALL
 
 python3 -u S_all.py -t $SLURM_ARRAY_TASK_ID
