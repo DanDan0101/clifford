4758fbaef1673649927ad5fe424c02a2303e623f
Daniel Sun <dansun@stanford.edu>
2023-08-14 00:55:06 -0700

    buff training specs

--- entropies.sh
+++ entropies.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=entropies
 #SBATCH --output=entropies.out
 #SBATCH --error=entropies.err
-#SBATCH --time=9:00:00
+#SBATCH --time=6:00:00
 #SBATCH -p hns
 #SBATCH --array=1-24
-#SBATCH -c 8
+#SBATCH -c 16
 #SBATCH --mem-per-cpu=4G
 #SBATCH --mail-type=ALL
 
