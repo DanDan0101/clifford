2c0fa7d1d3ed38a822fd880c26aa63206764a170
[Daniel Sun] <dansun@stanford.edu>
2023-08-16 18:36:34 -0700

    update batch bash

--- info.sh
+++ info.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/bash
 #SBATCH --job-name=info
-#SBATCH --output=info.out
-#SBATCH --error=info.err
-#SBATCH --time=3:00:00
+#SBATCH --time=12:00:00
 #SBATCH -p hns
-#SBATCH --array=1-985
-#SBATCH -c 8
-#SBATCH --mem-per-cpu=2G
+#SBATCH --array=1-330
+#SBATCH -c 16
+#SBATCH --mem-per-cpu=4G
 #SBATCH --mail-type=ALL
 
-python3 -u info.py -t $SLURM_ARRAY_TASK_ID
+python3 info.py -t $SLURM_ARRAY_TASK_ID
